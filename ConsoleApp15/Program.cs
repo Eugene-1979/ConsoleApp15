c2d05794a9ccb39434182e01e1bfee687d223642
yevhen_luchko@epam.com <yevhen_luchko@epam.com>
2025-01-30 20:19:02 +0200

    3

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World15!");
+            Console.WriteLine("Hello, World15!");
+            Console.WriteLine("Hello, World15!");
         }
     }
 }
